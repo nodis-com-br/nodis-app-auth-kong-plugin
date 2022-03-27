b5ac6e6bc9bb4f617cfa8f4d04c54f3a6c50b926
Victor Mesquita <78874691+victor-teles@users.noreply.github.com>
2022-03-27 15:10:14 -0300

    fixed table_find_pattern

--- kong/plugins/nodis-app-auth-kong-plugin/util.lua
+++ kong/plugins/nodis-app-auth-kong-plugin/util.lua
@@ -15,9 +15,9 @@ function _M.table_find(haystack, needle)
 end
 
 
-function _M.table_find_pattern(haystack, needle)
-  for k, v in pairs(haystack) do
-    local isMatching = not (string.find(ngx.var.uri, pattern) == nil)
+function _M.table_find_pattern(haystack, value)
+  for k, pattern in pairs(haystack) do
+    local isMatching = not (string.find(value, pattern) == nil)
     if (isMatching) then return true end
   end
 
